4eb841f87fac7346f3f9ab26c3226950bb342209
Ajay Kumar <ajaykumar8752124@gmail.com>
2025-04-22 00:37:22 +0530

    add all files

---
+++ loader/loader.js
@@ -0,0 +1,6 @@
+// Wait for the window to load
+window.addEventListener("load", function() {
+  // Hide the loader when the page is fully loaded
+  const loader = document.querySelector(".loader");
+  loader.style.display = "none";
+});
